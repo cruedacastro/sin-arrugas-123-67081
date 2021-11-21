30dd41d1692e712ca5c6b8e430bdfffc294df856
Stackbit Code Editor <projects@stackbit.com>
2021-11-21 01:25:31 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,7 +32,15 @@ export default class MyDocument extends Document {
         return (
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}</Head>
-                
+                  <script async src="https://www.googletagmanager.com/gtag/js?id=G-E6RCJCF492"></script>
+
+                <script dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', 'G-E6RCJCF492');`
+                }} />
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
                     <script src="/js/plugins.js" />
